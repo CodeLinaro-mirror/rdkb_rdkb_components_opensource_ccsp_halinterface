d73c7088d10664de5446ae1740c459cce6f5aaf2
Danil Chestyunin <dchestyunin@maxlinear.com>
2024-08-09 14:55:13 +0300

    RDKCOM-5088: RDKBDEV-2842  UL/DL MU stats are mixed up in wifi_associated_d...
    Reason for change:
    [halinterface] UL/DL MU stats are mixed up in wifi_associated_dev3_t
    Structure fields do not match with their types
     in wifi_associated_dev3_t
    Test Procedure: Check the build
    Risks: None
    Signed-off-by: dchestyunin@maxlinear.com
    Priority: P1
    
    (cherry picked from commit c293df8332bb45903cc0943a1bb92a8e848ff4e8)
    Change-Id: I943588f77eb3f8259e78a72abe05849c13bc9079

--- wifi_hal_generic.h
+++ wifi_hal_generic.h
@@ -1075,8 +1075,8 @@ typedef struct _wifi_associated_dev3
 
        UINT  cli_MaxDownlinkRate; /**< The Max data transmit rate in Mbps for the access point to the associated device. */
        UINT  cli_MaxUplinkRate;   /**<  The Max data transmit rate in Mbps for the associated device to the access point. */
-       wifi_ul_mu_stats_t  cli_DownlinkMuStats;
-       wifi_dl_mu_stats_t  cli_UplinkMuStats;
+       wifi_dl_mu_stats_t  cli_DownlinkMuStats;
+       wifi_ul_mu_stats_t  cli_UplinkMuStats;
        wifi_twt_dev_info_t cli_TwtParams; /**< TWT sessions that the device has joined */
 
        /* To facilitate retrieval of CSI data for specific associated client, an existing RDK-B Wi-Fi HAL 
