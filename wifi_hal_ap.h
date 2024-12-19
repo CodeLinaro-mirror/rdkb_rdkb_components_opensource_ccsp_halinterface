bae0d8a5f5a6aeb0623729a55f54966f7888a1f9
Pulluru, Harshavardhan <Harshavardhan_Pulluru@comcast.com>
2024-12-19 13:30:22 +0000

    RDKB-57977:Transient Client Management Configurations and RFC
    Impacted Platforms:
    OneWifi platforms
    
    Reason for change: Implement TCM Configurations,RFC as part of Epic
    
    Test Procedure: Load the build and test the changes as per ACs
    
    Risks: Low
    
    Signed-off-by: Harshavardhan_Pulluru@comcast.com
    Change-Id: I256d327949d14240ce07a86b6a9f7478ad22e838

--- wifi_hal_ap.h
+++ wifi_hal_ap.h
@@ -2617,6 +2617,11 @@ typedef struct {
     char minimum_advertised_mcs[32];
     char sixGOpInfoMinRate[32];
     char client_deny_assoc_info[45];
+    int  time_ms;
+    int  min_num_mgmt_frames;
+    char tcm_exp_weightage[32];
+    char tcm_gradient_threshold[32];
+    char tcm_client_deny_assoc_info[64];
     wifi_vap_name_t vap_name;
 } __attribute__((packed)) wifi_preassoc_control_t;
 
